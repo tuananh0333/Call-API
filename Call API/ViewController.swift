c3047806d7c95fcbf632e88d48051a342966e8a0
le tuan anh <tuananh0333@gmail.com>
2019-08-26 21:57:37 +0700

    first commit

--- Call API/ViewController.swift
+++ Call API/ViewController.swift
@@ -8,11 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct ResponseObject: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "userId"
+        case id = "id"
+        case title = "title"
+        case completed = "completed"
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var txtUserId: UITextField!
+    @IBOutlet weak var txtId: UITextField!
+    @IBOutlet weak var txtTittle: UITextField!
+    @IBOutlet weak var swCompleted: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url:String = "https://jsonplaceholder.typicode.com/todos/1?fbclid=IwAR07DUHEaO5AqxM1OuliRkbsaDVD1lJHo9_WRgjwRW9zqT2va_3IvyLof9Q"
+        
+        let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, _, error in
+            guard let data = data, error == nil else {
+                print(error ?? "Unknown error")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            var userId:Int?
+            var id:Int?
+            var title:String?
+            var completed:Bool?
+            
+            do {
+                let responseObject = try decoder.decode(ResponseObject.self, from: data)
+                print(responseObject)
+                userId = responseObject.userId
+                id = responseObject.id
+                title = responseObject.title
+                completed = responseObject.completed
+            } catch {
+                print("ERROR: ")
+                print(error)
+            }
+            
+            DispatchQueue.main.async {
+                self.txtUserId.text = String(userId!)
+                self.txtId.text = String(id!)
+                self.txtTittle.text = title
+                if(completed)! {
+                    self.swCompleted.isOn = true
+                } else {
+                    self.swCompleted.isOn = false
+                }
+            }
+        }
+        task.resume()
+
     }
 
     override func didReceiveMemoryWarning() {
